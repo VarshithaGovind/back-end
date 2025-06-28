5b06fca2cee91209a0e4a6caf11ebdadee2513bc
VarshithaGovind <167661585+VarshithaGovind@users.noreply.github.com>
2025-06-28 16:24:46 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,17 +2,13 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('✅ Database connected');
-  } catch (err) {
-    console.error('❌ Database connection failed');
-    console.error(err.message);
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('✅ Database connected');
+    } catch (err) {
+        console.error('❌ Database connection failed');
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
